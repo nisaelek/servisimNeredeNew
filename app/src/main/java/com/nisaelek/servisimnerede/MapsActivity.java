83a0800375fe7fee388e8c61d35f4607232d381c
msa585 <studi.msa@gmail.com>
2020-06-28 14:24:16 +0300

    tmm çalıştı

--- app/src/main/java/com/nisaelek/servisimnerede/MapsActivity.java
+++ app/src/main/java/com/nisaelek/servisimnerede/MapsActivity.java
@@ -112,16 +112,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         //afra sana cok teşekk
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
+
         mMap.setMyLocationEnabled(true);
 
 
